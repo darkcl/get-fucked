9fffac2652ee7e63cef69359ac66f37945a3396b
Yeung Yiu Hung <446404+darkcl@users.noreply.github.com>
2021-08-16 15:27:51 +0800

    feat(answer): add answer decorator

--- src/core/modules/alert/alert.create.dto.ts
+++ src/core/modules/alert/alert.create.dto.ts
@@ -1,6 +1,23 @@
+import { Answer } from "~lib/injector";
+
 export class AlertCreateDto {
+  @Answer({ question: "What is the title of the alert?", order: 0 })
   name: string;
+
+  @Answer({ question: "What is the name of the service?", order: 1 })
   service: string;
+
+  @Answer({
+    question: "What is the repositories should I listen to (comma separated)?",
+    order: 2,
+    handler: (answer: string) => answer.split(","),
+  })
   repositories: string[];
+
+  @Answer({
+    question: "What is the keywords should I listen to (comma separated)?",
+    order: 3,
+    handler: (answer: string) => answer.split(","),
+  })
   triggers: string[];
 }
--- src/lib/injector/decorator/answer.ts
+++ src/lib/injector/decorator/answer.ts
@@ -1,15 +1,22 @@
 export const ANSWERS_KEY = "answers";
 
+type AnswerHandler = (answer: string) => any;
+
 type AnswerDecoratorOptions = {
   question: string;
   order: number;
+  handler?: AnswerHandler;
 };
 
-export const Answer = ({ question, order }: AnswerDecoratorOptions) => {
-  return (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
+export const Answer = ({
+  question,
+  order,
+  handler = (answer) => answer,
+}: AnswerDecoratorOptions) => {
+  return (target: any, propertyKey: string) => {
     const answers = Reflect.getMetadata(ANSWERS_KEY, target.constructor) ?? {};
 
-    answers[propertyKey] = { order, question };
+    answers[propertyKey] = { order, question, handler };
 
     Reflect.defineMetadata(ANSWERS_KEY, answers, target.constructor);
   };
