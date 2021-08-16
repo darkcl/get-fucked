25a3e3e41bf865d8cf25772345a76ab6f59c420e
Yeung Yiu Hung <446404+darkcl@users.noreply.github.com>
2021-08-16 13:15:12 +0800

    feat(cmd): add guided context

--- src/lib/injector/decorator/command.ts
+++ src/lib/injector/decorator/command.ts
@@ -1,13 +1,20 @@
 export const COMMANDS_KEY = "commands";
 
-export const Command = (name: string = "") => {
+type CommandDecoratorOption = {
+  guidedContext?: boolean;
+};
+
+export const Command = (
+  name: string = "",
+  { guidedContext = false }: CommandDecoratorOption = {}
+) => {
   return (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
     let commands = Reflect.getMetadata(COMMANDS_KEY, target.constructor);
     if (!commands) {
       commands = {};
     }
 
-    commands[name] = { propertyKey };
+    commands[name] = { propertyKey, guidedContext };
 
     Reflect.defineMetadata(COMMANDS_KEY, commands, target.constructor);
   };
