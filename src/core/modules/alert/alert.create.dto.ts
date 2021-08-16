9cd4f91d29791f866629c9e9d6d15f1bc79b0641
Yeung Yiu Hung <446404+darkcl@users.noreply.github.com>
2021-08-16 17:01:29 +0800

    feat(dto): add short name

--- src/core/modules/alert/alert.create.dto.ts
+++ src/core/modules/alert/alert.create.dto.ts
@@ -4,19 +4,22 @@ export class AlertCreateDto {
   @Answer({ question: "What is the title of the alert?", order: 0 })
   name: string;
 
-  @Answer({ question: "What is the name of the service?", order: 1 })
+  @Answer({ question: "What is the short name of the alert?", order: 1 })
+  shortName: string;
+
+  @Answer({ question: "What is the name of the service?", order: 2 })
   service: string;
 
   @Answer({
     question: "What is the repositories should I listen to (comma separated)?",
-    order: 2,
+    order: 3,
     handler: (answer: string) => answer.split(","),
   })
   repositories: string[];
 
   @Answer({
     question: "What is the keywords should I listen to (comma separated)?",
-    order: 3,
+    order: 4,
     handler: (answer: string) => answer.split(","),
   })
   triggers: string[];
