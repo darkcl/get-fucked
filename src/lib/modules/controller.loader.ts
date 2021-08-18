3906e5b60c1696118793083ac2f741e5902e19de
Yeung Yiu Hung <446404+darkcl@users.noreply.github.com>
2021-08-18 22:34:31 +0800

    fix(module): fix duplicated controllers

--- src/lib/modules/controller.loader.ts
+++ src/lib/modules/controller.loader.ts
@@ -1,10 +1,16 @@
 import { MODULE_CONTROLLERS_KEY, MODULE_IMPORTS_KEY } from "~lib/modules";
 
-export const loadControllersFromModule = (module: NewableFunction) => {
+export const loadControllersFromModule = (
+  module: NewableFunction
+): NewableFunction[] => {
   const controllers = Reflect.getMetadata(MODULE_CONTROLLERS_KEY, module);
   const imports = Reflect.getMetadata(MODULE_IMPORTS_KEY, module);
   for (const importModule of imports) {
-    controllers.push(...loadControllersFromModule(importModule));
+    controllers.push(
+      ...loadControllersFromModule(importModule).filter(
+        (v) => controllers.indexOf(v) === -1
+      )
+    );
   }
   return controllers;
 };
