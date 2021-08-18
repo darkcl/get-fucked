47a39c03288a8190a365b5c83949a3954bb3ef0b
Yeung Yiu Hung <446404+darkcl@users.noreply.github.com>
2021-08-18 11:07:23 +0800

    doc(deck): update deck

---
+++ src/playground/person4.ts
@@ -0,0 +1,44 @@
+import "reflect-metadata";
+const requiredMetadataKey = Symbol("required");
+
+function Required(target, name, index) {
+  const required: number[] =
+    Reflect.getOwnMetadata(requiredMetadataKey, target, name) || [];
+  required.push(index);
+  Reflect.defineMetadata(requiredMetadataKey, required, target, name);
+}
+
+function Validate(target, name, descriptor) {
+  const method = descriptor.value!;
+
+  descriptor.value = function () {
+    const required: number[] = Reflect.getOwnMetadata(
+      requiredMetadataKey,
+      target,
+      name
+    );
+    if (required) {
+      for (let parameterIndex of required) {
+        if (
+          parameterIndex >= arguments.length ||
+          arguments[parameterIndex] === undefined
+        ) {
+          throw new Error("Missing required argument.");
+        }
+      }
+    }
+    return method.apply(this, arguments);
+  };
+}
+
+class Person {
+  constructor() {}
+
+  @Validate
+  live(@Required food?: string) {
+    console.log("Person live with ", food);
+  }
+}
+
+const person = new Person();
+person.live();
